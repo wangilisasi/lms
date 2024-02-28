1199cb029907e4cbf3e1512371e8b171f50c04f9
Emil Patrick <wangilisasi@gmail.com>
2024-02-28 11:01:43 +0300

    fixed CategoryForm.tsx error

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/CategoryForm.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/CategoryForm.tsx
@@ -103,7 +103,7 @@ const CategoryForm = ({
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <Combobox options={...options} {...field} />
+                    <Combobox options={options} {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
